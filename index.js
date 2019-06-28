5be932e618b56ba152cb5feda61795d3eb046cd2
belaifa el hussein <belaifahocine@gmail.com>
2019-06-28 18:30:07 +0200

    set up the socket to listen to status & fetch the messages

--- index.js
+++ index.js
@@ -5,5 +5,29 @@ const client = require("socket.io").listen(4000).sockets;
 
 mongo.connect("mongodb://localhost/mongochat", (err, db) => {
   if (err) throw err;
+
   console.log("connected to mongodb");
+
+  //connect to socket.io
+  client.on("connection", () => {
+    let chat = db.collection("chats");
+
+    //create a function to send status
+
+    sendStatus = s => {
+      client.emit("status", s);
+    };
+
+    //get chat from the collection
+
+    chat
+      .find()
+      .limit(100)
+      .sort({ _id: 1 })
+      .toArray((err, res) => {
+        if (err) throw err;
+        //emit the messages
+        client.emit("input", res);
+      });
+  });
 });
